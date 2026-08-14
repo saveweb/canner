12092607a37ff89fedbf727748ca8f3ea9d7995d
yzqzss <yzqzss@saveweb.org>
2026-08-14 06:08:06 +0200

    fix: interleave package builds and deliveries

--- delivery.go
+++ delivery.go
@@ -307,26 +307,25 @@ func (w *deliveryWorker) runScheduler(ctx context.Context) error {
 			nextReconcile = w.now().Add(deliveryReconcileInterval)
 		}
 
-		worked, err := w.runPackageBuildCycle(ctx)
+		built, err := w.runPackageBuildCycle(ctx)
 		if err != nil {
 			return stop(err)
 		}
-		if worked {
-			continue
-		}
-		worked, err = w.runPackagedSourcePurgeCycle(ctx)
-		if err != nil {
-			return stop(err)
-		}
-		if worked {
-			continue
-		}
-		worked, err = w.runPackagePurgeCycle(ctx)
-		if err != nil {
-			return stop(err)
-		}
-		if worked {
-			continue
+		if !built {
+			worked, err := w.runPackagedSourcePurgeCycle(ctx)
+			if err != nil {
+				return stop(err)
+			}
+			if worked {
+				continue
+			}
+			worked, err = w.runPackagePurgeCycle(ctx)
+			if err != nil {
+				return stop(err)
+			}
+			if worked {
+				continue
+			}
 		}
 
 		for active < w.cfg.DeliveryConcurrency {
